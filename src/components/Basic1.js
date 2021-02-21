2c6bc76657417fb1ed92812c8df69f1bc5ed2527
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-21 19:47:21 +0900

    リセット

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,12 +1,10 @@
 import React, {useState} from "react"
 
-const Basic1 = (props) => {
+const Basic1 = () => {
 	const [count, setCount] = useState(0)
 	
 	return (
 		<React.Fragment>
-			<button onClick={() => {setCount(prevCount=>prevCount+1);setCount(prevCount=>prevCount+1);}}>カウント{count}</button>
-			{ props.name }
 		</React.Fragment>
 	)
 }
