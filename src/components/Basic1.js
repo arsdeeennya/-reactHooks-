243bd4d822f360a3f5bf360cbabe2216909d6eed
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-22 20:58:26 +0900

    チェンジイベント実装

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,10 +1,16 @@
 import React, {useState} from "react"
 
 const Basic1 = () => {
-	const [count, setCount] = useState(0)
+	const [product, setProducts] = useState({name: "", price: ""})
 	
 	return (
 		<React.Fragment>
+			<form>
+				<input type="text" value={product.name} onChange={evt => setProducts({...product, name:evt.target.value})}/>
+				<input type="text" value={product.price} onChange={evt => setProducts({...product, price:evt.target.value})}/>
+			</form>
+			<h3>{product.name}</h3>
+			<h3>{product.price}</h3>
 		</React.Fragment>
 	)
 }
