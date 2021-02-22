29d268befb041cdf06d90a750d24226fde164c38
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-23 00:32:01 +0900

    初めてuseEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import React from 'react'
 import Basic2 from "./components/Basic2"
+import BasicUseEffect from "./components/BasicUseEffect"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Basic2/>
+        <BasicUseEffect/>
       </header>
     </div>
   );
--- src/components/BasicUseEffect.js
+++ src/components/BasicUseEffect.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 const BasicUseEffect = () => {
+	
+	const [count, setCount] = useState(0)
+	const [item, setItem] = useState("")
+	
+	
+	useEffect(() => {
+		console.log("aaaaaa")
+	},[count])
+	
 	return (
 		<React.Fragment>
+			<button onClick={() => setCount(prevCount=>prevCount+1)}>Click{count}</button>
+			<input type="text" value={item} onChange={evt=>setItem(evt.target.value)}/>
 		</React.Fragment>
 	)
 }
