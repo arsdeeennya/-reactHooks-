60950c2d941eaa644072a15108f0afce79dc913f
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-22 22:54:00 +0900

    useStateで配列操作

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg'
 import './App.css'
 import React from 'react'
-import Basic1 from "./components/Basic1"
+import Basic2 from "./components/Basic2"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Basic1 name="hooks"/>
+        <Basic2/>
       </header>
     </div>
   );
--- src/components/Basic2.js
+++ src/components/Basic2.js
@@ -1,9 +1,19 @@
 import React, {useState} from "react"
 
 const Basic2 = () => {
+	const [product, setProduct] = useState([])
+	const newProduct = () => {
+		setProduct([...product, {id: product.length, name: "Hello React"}])
+	}
+	
 	return (
 		<React.Fragment>
-			
+			<button onClick={newProduct}>New Product</button>
+			<ul>
+				{product.map(product => (
+					<li key={product.id}>{product.name} id: {product.id}</li>
+				))}
+			</ul>
 		</React.Fragment>
 	)
 }
