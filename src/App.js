bab4597a813f08f2af8cd6b2565d7a19d3c87750
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-23 10:02:25 +0900

    jsonplaceholderのapi叩く

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css'
 import React from 'react'
 // import Basic2 from "./components/Basic2"
 // import BasicUseEffect from "./components/BasicUseEffect"
-import TimerContainer from "./components/TimerContainer"
+// import TimerContainer from "./components/TimerContainer"
+import ApiFetch from "./components/ApiFetch"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TimerContainer/>
+        <ApiFetch/>
       </header>
     </div>
   );
