2f88d8f734e8293d374e4fab4afb47036b305064
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-23 18:58:28 +0900

    useRuduceとuseContextを組み合わせる＂ git commit -m useRuduceとuseContextを組み合わせる＂

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import './App.css'
-import React from 'react'
+import React, {useReducer} from 'react'
 // import Basic2 from "./components/Basic2"
 // import BasicUseEffect from "./components/BasicUseEffect"
 // import TimerContainer from "./components/TimerContainer"
 // import ApiFetch from "./components/ApiFetch"
 import AppContext from "./contexts/AppContext"
 // import B from "./components/B"
+import CompB from "./components/CompB"
 import BasicReducer from "./components/BasicReducer"
 
+const initialState = 0
+const reducer = (currentState, action) => {
+	switch(action) {
+		case "add_1":
+			return currentState + 1
+		case "multiple_3":
+			return currentState * 3
+		case "reset":
+			return initialState
+		default:
+			return currentState
+	}
+}
 
 function App() {
-  return (
-	<AppContext.Provider value={"こんちわ"}>
-	    <div className="App">
-	      <header className="App-header">
-	        <BasicReducer/>
-	      </header>
-	    </div>
-	</AppContext.Provider>
-  );
+	const [count, dispatch] = useReducer(reducer, initialState)
+	return (
+		<AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
+			<div className="App">
+				<header className="App-header">
+					Count{count}
+					<CompB/>
+				</header>
+			</div>
+		</AppContext.Provider>
+	);
 }
 
 export default App;
---
+++ src/components/CompB.js
@@ -0,0 +1,13 @@
+import React from "react"
+import CompC from "./CompC"
+
+
+const CompB = () => {
+	return (
+		<React.Fragment>
+			<CompC/>
+		</React.Fragment>
+	)
+}
+
+export default CompB;
